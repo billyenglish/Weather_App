6a6460350bf3ae92bb4fdc20d5718c1b60c74ce8
Billy English <billyenglish6212@outlook.com>
2025-05-12 03:06:41 -0400

    Testing for alternative solutions for the API.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-import "./styles/styles.sass";
+import "./styles/styles.scss";
 
+const currentLocation = 'Santa Barbara';
+
+const getWeather = async () => {
+    try {
+        const fetchWeather = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${currentLocation}?key=PD434M7WW9DBNZ57NE8YP34A2`, { mode: "cors"})
+        const weatherResult = await fetchWeather.json();
+        return weatherResult;
+    } catch (error) {
+        console.error("Error");
+    }
+}
+
+let result = await getWeather();
+console.dir(result);
