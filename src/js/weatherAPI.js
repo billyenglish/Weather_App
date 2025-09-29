48d3e261a2bc95512afe2573d7f416ce0c930da2
Billy English <billyenglish6212@outlook.com>
2025-09-29 01:54:18 -0400

    Weather module.

---
+++ src/js/weatherAPI.js
@@ -0,0 +1,14 @@
+const API_KEY = process.env.WEATHER_API_KEY;
+
+const weatherAPI = async (location = 'New York, NY') => {
+    try {
+        const search = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}`);
+        const data = await search.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching weather data:", error);
+        throw error;
+    }
+}
+
+export default weatherAPI;
