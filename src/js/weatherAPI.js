dcb6cb217dd48b4684ddc74e0f25042fc99faf0d
Billy English <billyenglish6212@outlook.com>
2025-10-03 15:57:23 -0400

    Added a default parameter for the weather api function.

--- src/js/weatherAPI.js
+++ src/js/weatherAPI.js
@@ -1,8 +1,8 @@
-const API_KEY = process.env.WEATHER_API_KEY;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
 const weatherAPI = async (location = 'New York, NY') => {
     try {
-        const search = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${API_KEY}`);
+        const search = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=${WEATHER_API_KEY}`);
         const data = await search.json();
         return data;
     } catch (error) {
